b364f21e2e341f8815841bfacc0a334ad9919b31
home <home@DESKTOP-V1JQBDI>
2023-03-05 05:49:07 +0900

    init

---
+++ src/main/java/com/study/front/web/HomeController.java
@@ -0,0 +1,61 @@
+package com.study.front.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+//	@GetMapping("index")
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@GetMapping("/home")
+	public String home() {
+		return "/home";
+	}
+	
+	@GetMapping("/testhtml")
+	public String testHtml() {
+		return "/testhtml";
+	}
+	
+	@GetMapping("/testcss")
+	public String testCss() {
+		return "/testcss";
+	}
+	
+	@GetMapping("/testjs")
+	public String testJs() {
+		return "/testjs";
+	}
+	
+	@GetMapping("/testjquery")
+	public String testJquery() {
+		return "/testjquery";
+	}
+	
+	@GetMapping("/testajax")
+	public String testAjax() {
+		return "/testajax";
+	}
+
+	@GetMapping("/testjsp")
+	public String testJsp() {
+		return "/testjsp";
+	}
+	
+	@GetMapping("/testresponsive")
+	public String testResponsive() {
+		return "/testresponsive";
+	}
+	
+	@GetMapping("/testweb")
+	public String testWeb() {
+		return "/testweb";
+	}
+
+	
+}
---
+++ src/main/resources/static/js/test.js
@@ -0,0 +1,7 @@
+
+
+function innerHtml() {
+  //document.getElementById("externaldemo").innerHTML = "Paragraph changed.";
+  return "Paragraph changed.";
+}
+
